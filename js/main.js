914eea4d446c22240049d00b7adfdb4026272113
asmith-asmith <aaronmeirsmith@gmail.com>
2020-05-15 10:57:49 -0700

    all

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-$('#add').click((event) =>{
-    console.log(event)
-    console.log('++++++')
-    console.log(this)
-    console.log('++++++')
-    console.log(event.target)
-})
+
+$(document).ready(function() {
+
+    $('#add').click((event) =>{
+        
+        var bla = $('#myInput').val();
+        var newRow =  `<tr>
+                        <td><button class="btn btn-xs btn-danger">X</button></td>
+                        <td>${bla}</td>
+                    </tr>`;
+        $('#skills tbody').append(newRow);
+    })
+
+    $('#skills tbody').on('click', 'button', function(e) {
+        $(e.target).closest('tr').remove();
+    });
+
+});
